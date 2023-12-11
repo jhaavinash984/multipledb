eadb4f1fc6273e1c6c86a91fe09f4f1b9b733ebe
avijha2 <avijha2@cisco.com>
2023-12-11 20:28:03 +0530

    Deployment Issue

---
+++ setenv.sh
@@ -0,0 +1,5 @@
+export CATALINA_OPTS="$CATALINA_OPTS -Djava.library.path=/usr/lib/oracle/11.2/client64/lib"
+export ORACLE_HOME="/usr/lib/oracle/11.2/client64"
+export LD_LIBRARY_PATH='/usr/lib/oracle/11.2/client64/lib'
+export TNS_ADMIN='/usr/lib/oracle/11.2/network/admin'
+export CLASSPATH='/usr/lib/oracle/11.2/client64/lib/ojdbc6.jar'
